1d8b4b62811efd658c3c7c43fd5d901c24689431
Kevin4080138 <doctorarabboyev@gmail.com>
2026-06-21 14:27:02 +0500

    Shikoyatlar ro'yxatini kengaytirish (siydik miqdori, jinsiy aloqa, bepushtlik va h.k.)

--- src/lib/shikoyatlar.ts
+++ src/lib/shikoyatlar.ts
@@ -4,42 +4,56 @@ export const shikoyatToifalari: { nom: string; shikoyatlar: string[] }[] = [
     shikoyatlar: [
       'Tez-tez siyish', 'Kechasi tez-tez siyish (nokturiya)', 'Siyishda achishish',
       'Siyish qiyinligi', 'Siydikni ushlab turolmaslik', 'Siydik oqimi zaifligi',
-      'Siydikda qon (gematuriya)', 'Siyishdan keyin tomchilash',
+      'Siydik oqimi ikkiga bo\'linishi', 'Siyishda kuchanish', 'Siydikda qon (gematuriya)',
+      'Siyishdan keyin tomchilash', 'Siydik miqdori kamligi (oliguriya)',
+      'Siydik miqdori ko\'pligi (poliuriya)', 'Siydik rangi o\'zgarishi',
+      'Siydik hidi o\'zgarishi', 'Siydik loyqaligi',
     ],
   },
   {
     nom: 'Buyrak',
     shikoyatlar: [
-      "Bel og'rig'i", 'Buyrak sohasida og\'riq', 'Buyrak sohasida og\'riq (kolikasimon)',
-      "Harorat ko'tarilishi", 'Shish (oyoq/yuzda)',
+      "Bel og'rig'i", "Buyrak sohasida og'riq", "Buyrak sohasida og'riq (kolikasimon)",
+      "Harorat ko'tarilishi", 'Shish (oyoq/yuzda)', 'Buyrakda tosh shikoyati',
+      'Siydik tahlilida o\'zgarish aniqlangan',
     ],
   },
   {
     nom: 'Qovuq',
     shikoyatlar: [
       "Qovuq to'la his qilish", "Qovuqni to'liq bo'shata olmaslik", "Qov usti og'rig'i",
-      "Qovuqda achishish",
+      "Qovuqda achishish", "Qovuqda tosh shikoyati", "Qovuq infeksiyasiga xos belgilar",
     ],
   },
   {
     nom: 'Prostata bezi',
     shikoyatlar: [
       "Prostata sohasida og'riq", "Siyishni boshlashda qiyinchilik", "Siyish oqimining zaiflashishi",
-      "To'liq bo'shamaganlik hissi", "Chov sohasida noqulaylik",
+      "To'liq bo'shamaganlik hissi", "Chov sohasida noqulaylik", "Prostatitga xos og'riq (oraliq sohada)",
+      "Ejakulyatsiyadan keyin og'riq",
     ],
   },
   {
     nom: 'Jinsiy aloqa',
     shikoyatlar: [
-      'Erektil disfunksiya', 'Erta ejakulyatsiya', 'Jinsiy aloqada og\'riq',
-      'Libido pasayishi', "Bepushtlik bo'yicha shikoyat",
+      'Erektil disfunksiya', 'Ereksiyani saqlay olmaslik', 'Erta ejakulyatsiya',
+      'Jinsiy aloqa davomiyligi qisqaligi', 'Jinsiy aloqaga qiziqish (libido) pasayishi',
+      'Jinsiy aloqada og\'riq', 'Ejakulyatsiya qiyinligi', 'Spontan ereksiya yo\'qligi',
     ],
   },
   {
     nom: 'Moshonka / Tuxumdon',
     shikoyatlar: [
       "Moshonkada og'riq", "Moshonkada shish", "Varikotsele belgilari",
-      "Tuxumdonda og'riq",
+      "Tuxumdonda og'riq", "Tuxumdon hajmi kichikligi", "Tuxumdon assimetriyasi",
+    ],
+  },
+  {
+    nom: 'Bepushtlik / reproduktiv',
+    shikoyatlar: [
+      'Birinchi farzandsizlik (1 yildan ortiq)', 'Ikkinchi farzandsizlik (sekundar)',
+      'Turmush o\'rtog\'ida homiladorlik bo\'lmasligi', 'Spermogramma natijasi yomon',
+      'Avvalgi davolanish samarasiz bo\'lgan',
     ],
   },
 ]
